8d3d3f2944aaf1ff2b50355ab4a98c857ea3d641
Danimar Varisa <danimar@inovadora.com.br>
2022-06-07 11:52:08 -0300

    Add Breed resources and call breeds from the CatAPI

--- rails-app/db/seeds.rb
+++ rails-app/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#user = User.create(username: "admin", password: "admin", age: 32)
+
+def get_api_data
+    response = RestClient.get 'https://api.thecatapi.com/v1/breeds'
+    json_data = JSON.parse response
+
+    if json_data.nil?
+        json_data["data"].map do |breed|
+            Breed.create(
+                name: "#{breed["name"]}"
+            )
+        end
+    else
+        puts "error seeding breeds"
+    end
+end
